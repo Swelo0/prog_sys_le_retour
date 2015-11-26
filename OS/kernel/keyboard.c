c0f8ca9360d5065c6c3d014eda122cf82872fdee
DomKiki <DomKiki@hotmail.com>
2015-11-26 10:47:03 +0100

    Ajout fichiers
    Fichiers timer.c, timer.h et keyboard.c
    Makefile mis à jour

---
+++ OS/kernel/keyboard.c
@@ -0,0 +1,12 @@
+void keyboard_init() {
+	// Code was here
+}
+
+void keyboard_handler() {
+	// Code was here
+}
+
+int getc() {
+	// Code was here
+	return 0;
+}
---
+++ OS/kernel/timer.c
@@ -0,0 +1,20 @@
+#include "timer.h"
+
+void timer_init(uint32_t freq_hz) {
+	// Code was here
+}
+
+void timer_handler() {
+	// Code was here
+}
+
+uint get_ticks() {
+	// Code was here
+	return 0;
+}
+
+void sleep(uint ms) {
+	// Code was here
+}
+
+
